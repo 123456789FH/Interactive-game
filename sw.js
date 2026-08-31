da67fb7c2ec73614dc03471cbe839653c54b9f3e
123456789FH <212942390+123456789FH@users.noreply.github.com>
2026-08-31 16:35:56 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='place-value-treasure-v2-2-clean';
+const CACHE='place-value-treasure-v2-3-integrated-values';
 const ASSETS=['./','./index.html','./styles.css','./game.js','./manifest.webmanifest','./assets/treasure-bg.png','./assets/icon-192.png','./assets/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
